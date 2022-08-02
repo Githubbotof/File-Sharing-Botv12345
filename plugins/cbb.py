e4da8faf0330f97bde7e52efad153dd55c804ba6
Githubbotof <89619464+Githubbotof@users.noreply.github.com>
2022-08-02 08:14:15 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -16,7 +16,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 [
                     [
                         InlineKeyboardButton("🔒 Close", callback_data = "close"),
-                        InlineKeyboardButton('👩‍🦯 Back', callback_data='back'),
+                        InlineKeyboardButton('👩‍🦯 Back', callback_data='start'),
                     ]
                 ]
             )
